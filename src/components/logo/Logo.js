0d5e14cec0ef872875fb796c2a4e14ba340a0f9a
nmcneany <nmcneany>
2022-05-06 17:16:25 -0500

    assets

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,3 +1,9 @@
+import {
+  Image,
+  StyleSheet,
+  View
+} from 'react-native'
+
 import global from '../../globals/styles'
 
 import AcmeText from '../acme-text/AcmeText'
@@ -5,16 +11,35 @@ import AcmeText from '../acme-text/AcmeText'
 
 const Logo = ({ size, ...props }) => {
   return (
-    <AcmeText
-      {...props}
-      style={{
-        color: global.colors.primary,
-        fontSize: size
-      }}
-    >
-      Resibee
-    </AcmeText>
+    <>
+      <View>
+        <Image
+          style={styles.image}
+          resizeMode="contain"
+          source={require('@assets/beelogo.png')}
+        />
+        <AcmeText
+          {...props}
+          style={{
+            color: global.colors.primary,
+            fontSize: size
+          }}
+        >
+          Resibee
+        </AcmeText>
+      </View>
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  image: {
+    bottom: 75,
+    left: 40,
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  }
+})
 
 export default Logo
